c7cb58a567a262af1cee22ffa9d2a7697ad3fbea
IT20090562 <88573129+IT20090562@users.noreply.github.com>
2021-09-30 01:19:35 +0530

    shop order

--- app/src/main/java/com/example/madprojectdelta/shop_show_products.java
+++ app/src/main/java/com/example/madprojectdelta/shop_show_products.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Order_details extends AppCompatActivity {
+public class shop_show_products extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_order_details);
+        setContentView(R.layout.activity_shop_show_products);
     }
 }
